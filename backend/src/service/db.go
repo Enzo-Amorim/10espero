6cd3ebdb59c325ca7fc8975a9d1d74cd7238b4e1
Enzo <amorimenzo021@gmail.com>
2021-01-11 17:31:15 -0300

    Update db.go

--- backend/src/service/db.go
+++ backend/src/service/db.go
@@ -9,11 +9,13 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var mongoURI = "PUT YOUR KEY HERE"
+
 func GetConnection() *mongo.Client {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		"mongodb+srv://admin:1401@cluster0.6u86t.mongodb.net/10espero?retryWrites=true&w=majority",
+		mongoURI,
 	))
 	if err != nil {
 		log.Fatal(err)
